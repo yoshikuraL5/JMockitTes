8197f4aea9f618ae13d56843f7306bd899d47a62
yoshikura <yoshikura@192.168.116.6>
2014-04-25 20:11:41 +0900

    test

---
+++ JMockitTest/src/main/java/jmockit/App.java
@@ -0,0 +1,13 @@
+package jmockit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlds!" );
+    }
+}
